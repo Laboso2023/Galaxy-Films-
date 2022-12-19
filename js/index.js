e41d6beadbcec6036500f198c684eccfc897cfad
Sirumba <simonsmaina45@gmail.com>
2022-12-19 22:45:08 +0300

    adds the main section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 //created a nav-feature to allow for sleek scroll 
+// let nav = document.querySelector('nav');
 
+//       window.addEventListener('scroll', function () {
+//         if (window.pageYOffset > 100) {
+//           nav.classList.add('bg-dark', 'shadow');
+//         } else {
+//           nav.classList.remove('bg-dark', 'shadow');
+//         }
+//       });
+
+//      const button = document.getElementById('button')
+//      const bodyContent = document.getElementById('lds-roller')
+//      const dataInput = document.getElementsBy
 

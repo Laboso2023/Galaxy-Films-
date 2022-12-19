b7da072e424628a8306131ecdfea92a2a32b8962
Sirumba <simonsmaina45@gmail.com>
2022-12-20 01:51:15 +0300

    adds transition of overview

--- js/index.js
+++ js/index.js
@@ -12,4 +12,8 @@
 //      const button = document.getElementById('button')
 //      const bodyContent = document.getElementById('lds-roller')
 //      const dataInput = document.getElementsBy
+const baseUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5c6898b87a119919b82f3da7cb07bc46&page=1'
+const imagePath = 'https://image.tmdb.org/t/p/w1280/';
+const searchAPI = "https://api.themoviedb.org/3/search/movie?api_key=5c6898b87a119919b82f3da7cb07bc46&query="
 
+const form
